617d1ffb019dacaecfd479aed7137f4debcc6435
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-04-07 11:26:29 +0200

    algo

---
+++ game/gameManager.js
@@ -0,0 +1,22 @@
+let board;
+
+
+function traduction(boardString){
+    board= [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ];
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 8; j++) {
+            if(boardString[i*7+j]==='m')
+                board=1
+            else if (boardString[i*7+j]==='h')
+                board=-1
+        }
+    }
+}
