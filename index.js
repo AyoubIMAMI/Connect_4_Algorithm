2a49c75abc4eb8c1c2a4978e3e444bf87afe5e4e
Ayoub <youbima17@gmail.com>
2023-04-07 13:49:39 +0200

    api, call for the move

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ app.get('/move', async (request, response) => {
     const board = request.query['b'];
 
     if (!inputManager.isBoardValid(board))
-        return response.status(400).send("Invalid Format");
+        return response.status(400).send("Invalid board format");
 
     if(!inputManager.isBoardLegal(board))
-        return response.status(400).send("Illegal board");
+        return response.status(400).send("Illegal board: token floating in the air");
 
     let moveDifference = inputManager.moveCounter(board);
     if(moveDifference === 0) response.status(422).json({detail: "Not the turn of the AI."});
-    if(moveDifference > 1 || moveDifference < 0) return response.status(400).send("Illegal board");
+    else if(moveDifference > 1 || moveDifference < 0) return response.status(400).send("Illegal board");
 
-
+    try {
+        let column = await gameManager.play(board);
+        response.status(200).json({column: column});
+    } catch (e) {
+        response.status(422).json({detail: e.message});
+    }
 });
 
 app.listen(3000, () => {
