e67184b0e883978c314f2b3fd73cd96fb1ca3306
Ayoub <youbima17@gmail.com>
2023-04-17 14:24:15 +0200

    start script modification

--- back/start.sh
+++ back/start.sh
@@ -3,3 +3,5 @@ docker build -f Dockerfile -t connect4 .
 
 echo "-------------------- Up the container --------------------"
 docker compose up -d
+
+read -p "Press any key to quit (the container will still run)..."
